bd01786fb3ebac5d0086f2bb86850c8c47c76f96
Jey <jeremycastillo901@gmail.com>
2023-04-27 18:22:24 -0400

    TipoAplication Metodo Get

--- ProyectoWeb/Controllers/TipoAplicacionController.cs
+++ ProyectoWeb/Controllers/TipoAplicacionController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoWeb.Datos;
+using ProyectoWeb.Models;
 
 namespace ProyectoWeb.Controllers
 {
     public class TipoAplicacionController : Controller
     {
+        private ApplicationDbContext _applicationDbContext;
+
+        public TipoAplicacionController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<TipoAplicacion> lista = _applicationDbContext.TipoAplicacion;
+            return View(lista);
         }
+
+
     }
 }
+
+
+
