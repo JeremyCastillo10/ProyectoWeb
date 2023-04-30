c48eebbdcd54f81a972bd59c8ca354db835969ca
Jey <jeremycastillo901@gmail.com>
2023-04-29 23:54:47 -0400

    validating fields in categoria

--- ProyectoWeb/Models/Categoria.cs
+++ ProyectoWeb/Models/Categoria.cs
@@ -6,7 +6,12 @@ namespace ProyectoWeb.Models
     {
         [Key]
         public int Id  { get; set; }
+
+        [Required(ErrorMessage ="Nombre de categoria Obligatorio")]
         public string? NombreCategoria { get; set; }
+
+        [Required(ErrorMessage = "Orden Obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage ="El orden debe ser mayor a cero")]
         public int MostrarOrden { get; set; }
     }
 }
