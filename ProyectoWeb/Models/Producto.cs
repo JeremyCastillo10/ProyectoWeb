78008246978f38d72beb894607e3fa20207f9916
Jey <jeremycastillo901@gmail.com>
2023-05-01 16:07:13 -0400

    creating new model Producto

---
+++ ProyectoWeb/Models/Producto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoWeb.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El nombre es Obligatorio")]
+        public string NombreProducto { get; set; }
+        [Required(ErrorMessage = "La descripcion corta es Obligatoria")]
+        public string DescripcionCorta { get; set; }
+        [Required(ErrorMessage = "La descripcion del producto es obligatoria")]
+        public string DescripcionProducto { get; set; }
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        [Range(1, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+        public double Precio { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+        //Foreign Key
+
+        public int CategoriaId { get; set; }
+        [ForeignKey("CategoriaId")]
+        public virtual Categoria Categoria { get; set; }
+
+        public int TipoAplicacionId { get; set; }
+        [ForeignKey("TipoAplicacionId")]
+        public virtual TipoAplicacion TipoAplicacion { get; set; }
+    }
+}
