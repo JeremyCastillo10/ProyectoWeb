63fbc4846f9ac8fbc19383663ab075867f595ddd
Jey <jeremycastillo901@gmail.com>
2023-05-01 21:10:53 -0400

    creating viewmodel

---
+++ ProyectoWeb/Models/VIewModels/ProductoVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ProyectoWeb.Models.VIewModels
+{
+    public class ProductoVM
+    {
+        public Producto Producto { get; set; }
+        public IEnumerable<SelectListItem> CategoriaLista { get; set; }
+        public IEnumerable<SelectListItem> TipoAplicacionLista { get; set; }
+    }
+}
