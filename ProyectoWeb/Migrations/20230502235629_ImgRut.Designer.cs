86196ce3a65e0306c2a3f4f52258934950d16e81
Jey <jeremycastillo901@gmail.com>
2023-05-02 22:01:27 -0400

    migration ImagenRuta

--- ProyectoWeb/Migrations/20230502235629_ImgRut.Designer.cs
+++ ProyectoWeb/Migrations/20230502235629_ImgRut.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoWeb.Datos;
 
@@ -10,9 +11,11 @@ using ProyectoWeb.Datos;
 namespace ProyectoWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230502235629_ImgRut")]
+    partial class ImgRut
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
