bca0beb14c19034d9e8a6cd871c7516f2f7261cb
Jey <jeremycastillo901@gmail.com>
2023-05-02 22:01:39 -0400

    creating upsert method

--- ProyectoWeb/Controllers/ProductoController.cs
+++ ProyectoWeb/Controllers/ProductoController.cs
@@ -10,16 +10,19 @@ namespace ProyectoWeb.Controllers
     public class ProductoController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;   
 
-        public ProductoController(ApplicationDbContext context)
+        public ProductoController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            return View(await _context.Producto.Include(p => p.Categoria)
-                .Include(p => p.TipoAplicacion).ToListAsync());
+            IEnumerable<Producto> lista = _context.Producto.Include(c => c.Categoria)
+                                                      .Include(t => t.TipoAplicacion);
+            return View(lista);
         }
 
         public IActionResult Upsert(int? Id)
@@ -34,33 +37,107 @@ namespace ProyectoWeb.Controllers
             ProductoVM productoVM = new ProductoVM()
             {
                 Producto = new Producto(),
-                CategoriaLista = _context.Categoria.Select(c => new SelectListItem{
+                CategoriaLista = _context.Categoria.Select(c => new SelectListItem
+                {
                     Text = c.NombreCategoria,
                     Value = c.Id.ToString()
                 }),
-                TipoAplicacionLista = _context.TipoAplicacion.Select(c => new SelectListItem{
+                TipoAplicacionLista = _context.TipoAplicacion.Select(c => new SelectListItem
+                {
                     Text = c.Nombre,
                     Value = c.Id.ToString()
-                }),
-
-
-
+                })
             };
 
-            if(Id == null)
+            if (Id == null)
             {
+                // Crear un Nuevo Producto
                 return View(productoVM);
             }
             else
             {
                 productoVM.Producto = _context.Producto.Find(Id);
-                if(productoVM.Producto == null)
+                if (productoVM.Producto == null)
                 {
                     return NotFound();
                 }
-                return View(productoVM.Producto);
+                return View(productoVM);
             }
 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]  
+        public IActionResult Upsert(ProductoVM productoVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                string webRootPath = _webHostEnvironment.WebRootPath;
+                if (productoVM.Producto.Id == 0)
+                {
+                    // Crear
+                    string upload = webRootPath + WC.ImagenRuta;
+                    string fileName = Guid.NewGuid().ToString();
+                    string extension = Path.GetExtension(files[0].FileName);
+
+                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+
+                    productoVM.Producto.ImagenUrl = fileName + extension;
+                    _context.Producto.Add(productoVM.Producto);
+                }
+                else
+                {
+                    // Actualizar
+                    var objProducto = _context.Producto.AsNoTracking().FirstOrDefault(p => p.Id == productoVM.Producto.Id);
+
+                    if (files.Count > 0)  // Se carga nueva Imagen
+                    {
+                        string upload = webRootPath + WC.ImagenRuta;
+                        string fileName = Guid.NewGuid().ToString();
+                        string extension = Path.GetExtension(files[0].FileName);
+
+                        // borrar la imagen anterior
+                        var anteriorFile = Path.Combine(upload, objProducto.ImagenUrl);
+                        if (System.IO.File.Exists(anteriorFile))
+                        {
+                            System.IO.File.Delete(anteriorFile);
+                        }
+                        // fin Borrar imagen anterior
+
+                        using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStream);
+                        }
+
+                        productoVM.Producto.ImagenUrl = fileName + extension;
+                    }  // Caso contrario si no se carga una nueva imagen
+                    else
+                    {
+                        productoVM.Producto.ImagenUrl = objProducto.ImagenUrl;
+                    }
+                    _context.Producto.Update(productoVM.Producto);
+
+                }
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }  // If ModelIsValid
+            // Se llenan nuevamente las listas si algo falla
+            productoVM.CategoriaLista = _context.Categoria.Select(c => new SelectListItem
+            {
+                Text = c.NombreCategoria,
+                Value = c.Id.ToString()
+            });
+            productoVM.TipoAplicacionLista = _context.TipoAplicacion.Select(c => new SelectListItem
+            {
+                Text = c.Nombre,
+                Value = c.Id.ToString()
+            });
+
+            return View(productoVM);
         }
     }
 }
