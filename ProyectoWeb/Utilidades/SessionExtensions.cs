a8497e68c9c2a088f894baa4f0bed09587ae87ea
Jey <jeremycastillo901@gmail.com>
2023-05-06 14:16:10 -0400

    Configurate session

---
+++ ProyectoWeb/Utilidades/SessionExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+using Newtonsoft.Json;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace ProyectoWeb.Utilidades
+{
+    public static class SessionExtensions
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonSerializer.Deserialize<T>(value);   
+        }
+    }
+}
