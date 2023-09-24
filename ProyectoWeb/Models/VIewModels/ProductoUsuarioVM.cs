018ff204c2af2b5e2740dafe1492b81f994327d0
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-09-24 11:09:32 -0400

    creating new viewmodel

---
+++ ProyectoWeb/Models/VIewModels/ProductoUsuarioVM.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoWeb.Models.VIewModels
+{
+    public class ProductoUsuarioVM
+    {
+        public ProductoUsuarioVM()
+        {
+            ProductoLista = new List<Producto>();
+        }
+        public UsuariosAplicacion UsuariosAplicacion { get; set; }
+        IEnumerable<Producto> ProductoLista { get; set; }
+    }
+}
