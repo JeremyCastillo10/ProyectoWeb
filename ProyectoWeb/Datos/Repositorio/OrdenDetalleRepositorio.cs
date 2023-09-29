70bbcceb7ceb56b4f172add7dbd35415807fecb6
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-09-29 19:03:53 -0400

    Implementing interfaz and Repo From ordendetail

---
+++ ProyectoWeb/Datos/Repositorio/OrdenDetalleRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using ProyectoWeb.Datos.Repositorio.IRepositorio;
+using ProyectoWeb.Models;
+
+namespace ProyectoWeb.Datos.Repositorio
+{
+    public class OrdenDetalleRepositorio : Repositorio<OrdenDetalle>, IOrdenDetalleRepositorio
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrdenDetalleRepositorio(ApplicationDbContext context):base(context) 
+        {
+            _context = context;
+        }
+
+        public void Actualizar(OrdenDetalle ordenDetalle)
+        {
+            _context.Update(ordenDetalle);
+        }
+    }
+}
