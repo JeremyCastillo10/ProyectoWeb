d9bc25da432e9e71e3cf32641e95ef39be2c860b
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-09-29 19:05:07 -0400

    implementing interface and repo from usuarioaplicacion

---
+++ ProyectoWeb/Datos/Repositorio/IRepositorio/IUsuariosAplicacion.cs
@@ -0,0 +1,9 @@
+﻿using ProyectoWeb.Models;
+
+namespace ProyectoWeb.Datos.Repositorio.IRepositorio
+{
+    public interface IUsuariosAplicacion: IRepositorio<UsuariosAplicacion>
+    {
+        void Actualizar(UsuariosAplicacion usuariosAplicacion); 
+    }
+}
---
+++ ProyectoWeb/Datos/Repositorio/UsuariosAplicacionRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using ProyectoWeb.Datos.Repositorio.IRepositorio;
+using ProyectoWeb.Models;
+
+namespace ProyectoWeb.Datos.Repositorio
+{
+    public class UsuariosAplicacionRepositorio : Repositorio<UsuariosAplicacion>, IUsuariosAplicacion
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsuariosAplicacionRepositorio(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public void Actualizar(UsuariosAplicacion usuariosAplicacion)
+        {
+            _context.Update(usuariosAplicacion);    
+        }
+    }
+}
