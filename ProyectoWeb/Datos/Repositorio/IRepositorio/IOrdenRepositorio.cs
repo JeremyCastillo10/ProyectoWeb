7725d2953899640499a24bedb56bb4ec27d0987c
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-09-29 19:04:31 -0400

    implementing interface and repo from orden

---
+++ ProyectoWeb/Datos/Repositorio/IRepositorio/IOrdenRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using ProyectoWeb.Models;
+
+namespace ProyectoWeb.Datos.Repositorio.IRepositorio
+{
+    public interface IOrdenRepositorio:IRepositorio<Orden>
+    {
+        void Actualizar(Orden orden);
+    }
+}
---
+++ ProyectoWeb/Datos/Repositorio/OrdenRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using ProyectoWeb.Datos.Repositorio.IRepositorio;
+using ProyectoWeb.Models;
+
+namespace ProyectoWeb.Datos.Repositorio
+{
+    public class OrdenRepositorio : Repositorio<Orden>, IOrdenRepositorio
+    {
+        private readonly ApplicationDbContext _context;
+        public OrdenRepositorio(ApplicationDbContext context):base(context) 
+        {
+            _context = context;
+        }
+
+        public void Actualizar(Orden orden)
+        {
+            _context.Update(orden);
+        }
+    }
+}
