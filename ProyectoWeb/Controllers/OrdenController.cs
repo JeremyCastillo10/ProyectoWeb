b2a0c996c708872e97cfd5e57afafc69c8896bb7
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-10-01 19:26:52 -0400

    convert to shopping cart

--- ProyectoWeb/Controllers/OrdenController.cs
+++ ProyectoWeb/Controllers/OrdenController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProyectoWeb.Datos.Repositorio.IRepositorio;
+using ProyectoWeb.Models;
+using ProyectoWeb.Models.VIewModels;
+using ProyectoWeb.Utilidades;
 
 namespace ProyectoWeb.Controllers
 {
@@ -7,6 +10,9 @@ namespace ProyectoWeb.Controllers
     {
         private readonly IOrdenRepositorio _ordenRepo;
         private readonly IOrdenDetalleRepositorio _ordenDetalleRepo;
+
+        [BindProperty]
+        public OrdenVM OrdenVM { get; set; }
         public OrdenController(IOrdenRepositorio ordenRepo, IOrdenDetalleRepositorio ordenDetalleRepo)
         {
             _ordenRepo = ordenRepo;
@@ -16,6 +22,35 @@ namespace ProyectoWeb.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Detalle(int id) 
+        {
+            OrdenVM = new OrdenVM()
+            {
+                orden = _ordenRepo.ObtenerPrimero(o => o.Id == id),
+                ordenDetalles = _ordenDetalleRepo.ObtenerTodos(o=> o.OrdenId == id, incluirPropiedades:"Producto")
+            };
+            return View(OrdenVM);
+        }
+        [HttpPost]
+        public IActionResult Detalle() 
+        {
+            List<CarroCompra> carroComprasLista = new List<CarroCompra>();
+
+            OrdenVM.ordenDetalles = _ordenDetalleRepo.ObtenerTodos(o => o.OrdenId == OrdenVM.orden.Id);
+            foreach(var detalle in OrdenVM.ordenDetalles)
+            {
+                CarroCompra carroCompra = new CarroCompra() 
+                {
+                    ProductoId = detalle.ProductoId
+                };
+                carroComprasLista.Add(carroCompra);
+            }
+            HttpContext.Session.Clear();
+            HttpContext.Session.Set(WC.SessionCarroCompras, carroComprasLista);
+            HttpContext.Session.Set(WC.SessionOrdenId, OrdenVM.orden.Id);
+
+            return RedirectToAction("Index", "Carro");
         }
         #region APIS
         [HttpGet]
--- ProyectoWeb/WC.cs
+++ ProyectoWeb/WC.cs
@@ -6,10 +6,12 @@ namespace ProyectoWeb
     {
         public const string ImagenRuta = @"\imagenes\producto\";
         public const string SessionCarroCompras = "SessionCarroCompras";
+        public const string SessionOrdenId = "SessionOrden";
         public const string AdminRole = "Admin";
         public const string UserRole = "Cliente";
 
         public const string CategoriaNombre = "Categoria";
         public const string TipoAplicacionNombre = "TipoAplicacion";
+
     }
 }
