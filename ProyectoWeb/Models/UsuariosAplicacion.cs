cab88191c30ad751608b12c4f8971c6c09afc25c
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-10-06 19:07:59 -0400

    new fields ciudad and direction and validating fields with javascript

--- ProyectoWeb/Models/UsuariosAplicacion.cs
+++ ProyectoWeb/Models/UsuariosAplicacion.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProyectoWeb.Models
 {
     public class UsuariosAplicacion:IdentityUser
     {
         public string NombreCompleto { get; set; }
+
+        [NotMapped]
+        public string Direccion { get; set; }
+
+        [NotMapped]
+        public string Ciudad { get; set; }
     }
 }
